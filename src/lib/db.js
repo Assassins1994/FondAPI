95c0284f2b1ec8cdfcb76426eb0c2df6c6a5bec7
Daniel Ortega Escandón <danieloe-1994@hotmail.com>
2018-11-16 21:49:05 -0600

    Generando una versiÃn

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connect = () =>  new Promise((resolve, reject) => {
-    mongoose.connect('mongodb://DanielOE:Petunia-1923@ds159737.mlab.com:59737/fondapi', {
+    mongoose.connect('mongodb://fondapiDan:Petunia-1923@ds159737.mlab.com:59737/fondapi', {
       useNewUrlParser: true 
     })
     const db = mongoose.connection
